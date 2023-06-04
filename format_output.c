2498a39aaacf9ab20aabadcf5ec036a94150f57a
GenjiFujimoto <dadra102@hhu.de>
2023-06-04 19:37:33 +0200

    Seperate entries with only one newline

--- format_output.c
+++ format_output.c
@@ -3,15 +3,26 @@
 
 int main()
 {
+    int prevseen = 0;
     static char *buf = NULL;
     static size_t size = 0;
     long n = 1;
     while(getline(&buf, &size, stdin) > 0)
     {
-	  if (n++ < 5) /* overflow possible */
+	  if (n++ < 5)
       	        continue;
 	  if (strncmp(buf,"-->", 3) == 0)
-		putchar('\n');
+	  {
+		if (!prevseen)
+		{
+		      prevseen = 1;
+		      putchar('\n');
+		}
+		else
+		{
+		      prevseen = 0;
+		}
+	  }
 	  else if (buf[0] != 10)
 		fputs(buf, stdout);
     }
