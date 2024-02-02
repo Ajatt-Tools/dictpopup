b7fdb11a9f852855db27f44e7d23008cd1958145
GenjiFujimoto <dadra102@hhu.de>
2024-02-02 09:14:27 +0100

    Remove comments

--- dictionarylookup.c
+++ dictionarylookup.c
@@ -8,12 +8,8 @@
 #include "dictionary.h"
 #include "readsettings.h"
 
-#define E(expr) CHECK((rc = (expr)) == MDB_SUCCESS)
-#define RES(err, expr) ((rc = expr) == (err) || (CHECK(!rc, #expr), 0))
-#define CHECK(test) ((test) ? (void)0 : ((void)fprintf(stderr, \
-						       "%s:%d: %s\n", __FILE__, __LINE__, mdb_strerror(rc)), abort()))
 #define MDB_CHECK(call) \
-	if ((rc = call) != MDB_SUCCESS) { \
+	if ((rc = call) != MDB_SUCCESS && rc != MDB_NOTFOUND) { \
 		printf("Database error: %s\n", mdb_strerror(rc)); \
 		exit(EXIT_FAILURE); \
 	}
@@ -26,19 +22,14 @@ MDB_txn *txn;
 void
 open_database()
 {
-	E(mdb_env_create(&env));
+	MDB_CHECK(mdb_env_create(&env));
 	mdb_env_set_maxdbs(env, 2);
 
-	E(mdb_env_open(env, cfg.db_path, MDB_RDONLY | MDB_NOLOCK | MDB_NORDAHEAD, 0664));
-	E(mdb_txn_begin(env, NULL, MDB_RDONLY, &txn));
+	MDB_CHECK(mdb_env_open(env, cfg.db_path, MDB_RDONLY | MDB_NOLOCK | MDB_NORDAHEAD, 0664));
+	MDB_CHECK(mdb_txn_begin(env, NULL, MDB_RDONLY, &txn));
 
 	MDB_CHECK(mdb_dbi_open(txn, "db1", MDB_DUPSORT | MDB_DUPFIXED, &dbi1));
 	MDB_CHECK(mdb_dbi_open(txn, "db2", MDB_INTEGERKEY, &dbi2));
-
-	/* E(mdb_env_create(&env)); */
-	/* E(mdb_env_open(env, "/home/daniel/.local/share/dictpopup/", MDB_RDONLY | MDB_NOLOCK | MDB_NORDAHEAD, 0664)); */
-	/* E(mdb_txn_begin(env, NULL, MDB_RDONLY, &txn)); */
-	/* E(mdb_dbi_open(txn, NULL, MDB_DUPSORT, &dbi)); */
 }
 
 void
@@ -48,10 +39,6 @@ close_database()
 	mdb_dbi_close(env, dbi1);
 	mdb_dbi_close(env, dbi2);
 	mdb_env_close(env);
-
-	/* mdb_txn_abort(txn); */
-	/* mdb_dbi_close(env, dbi); */
-	/* mdb_env_close(env); */
 }
 
 void
@@ -77,32 +64,12 @@ add_de_from_db_lookup(dictionary *dict, char *db_lookup)
 	{
 		len = strlen(db_lookup);
 		ptr_buffer[i] = db_lookup;
-		/* printf("[%i]: %s\n", i, ptr_buffer[i]); */
 		db_lookup += len + 1;
 	}
 
 	dictionary_copy_add(dict, (const dictentry) { .dictname = ptr_buffer[0], .kanji = ptr_buffer[1], .reading = ptr_buffer[2], .definition = ptr_buffer[3] });
 	return db_lookup;
 }
-
-/* void */
-/* print_data(MDB_val id) */
-/* { */
-/* 	MDB_val data; */
-/* 	MDB_CHECK(mdb_get(txn, dbi2, &id, &data)); */
-
-/* 	char uncompressed[5000]; */
-/* 	size_t len = unishox2_decompress_simple(data.mv_data, data.mv_size, uncompressed); */
-
-/* 	for (char *c = uncompressed; c != uncompressed + len; c++) */
-/* 	{ */
-/* 		if (*c) */
-/* 			printf("%c", *c); */
-/* 		else */
-/* 			printf("\n"); */
-/* 	} */
-/* 	printf("\n\n"); */
-/* } */
 
 void
 add_word_to_dict(dictionary *dict, char *word)
@@ -114,39 +81,19 @@ add_word_to_dict(dictionary *dict, char *word)
 
 	MDB_cursor *cursor;
 	MDB_CHECK(mdb_cursor_open(txn, dbi1, &cursor));
-
-	if ((rc = mdb_cursor_get(cursor, &key, &ids, MDB_SET)) == MDB_NOTFOUND)
-		return;
-	else
-		CHECK(rc == MDB_SUCCESS);
+	MDB_CHECK(mdb_cursor_get(cursor, &key, &ids, MDB_SET));
 
 	MDB_CHECK(mdb_cursor_get(cursor, &key, &ids, MDB_GET_MULTIPLE)); // Reads up to a page, i.e. max 1024 entries
 	for (int i = 0; i < ids.mv_size ; i += 4)
 	{
 		MDB_val id = { .mv_data = (unsigned char*)ids.mv_data + i, .mv_size = 4 };
 
-		/* print_data(id); */
-
 		MDB_val data;
 		MDB_CHECK(mdb_get(txn, dbi2, &id, &data));
-		char decompressed[5000];
+		char decompressed[5000]; // FIXME: This is obviously bad, have to lookup the proper way of decompressing
 		size_t len = unishox2_decompress_simple(data.mv_data, data.mv_size, decompressed);
 		decompressed[len] = '\0';
 
 		add_de_from_db_lookup(dict, decompressed);
 	}
-
-	/* MDB_val key = (MDB_val) { .mv_size = strlen(word), .mv_data = word }; */
-	/* MDB_val data; */
-	/* if (mdb_get(txn, dbi, &key, &data) != MDB_NOTFOUND) */
-	/* { */
-	/* 	char uncompressed_data[5000]; */
-	/* 	size_t uncompressed_data_len = unishox2_decompress_simple(data.mv_data, data.mv_size, uncompressed_data); */
-
-	/* 	print_entry(uncompressed_data, uncompressed_data_len); */
-
-	/* 	char* data_ptr = uncompressed_data; */
-	/* 	while ((data_ptr = add_de_from_db_lookup(dict, data_ptr)) < uncompressed_data + uncompressed_data_len) */
-	/* 		; */
-	/* } */
 }
