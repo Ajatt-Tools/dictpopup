e5a6449f703d59072564ff386bfdeb36e1f46b47
GenjiFujimoto <dadra102@hhu.de>
2024-04-04 15:01:41 +0200

    Fix files being symlinks

--- include/ankiconnectc.h
+++ include/ankiconnectc.h
@@ -1,1 +1,61 @@
-../../ankiconnectC/ankiconnectc.h
+#ifndef ANKICONNECTC_H
+#define ANKICONNECTC_H
+
+typedef struct {
+  char *deck;
+  char *notetype;
+
+  size_t num_fields;
+  char **fieldnames;   
+  char **fieldentries; 
+  char **tags;
+} ankicard;
+
+void ankicard_free(ankicard ac);
+
+/*
+ * Print the contents of an ankicard 
+ */
+void ac_print_ankicard(ankicard ac);
+
+typedef struct {
+      union {
+          char* string;
+          char** stringv;
+          _Bool boolean;
+      } data;
+      _Bool ok; // Signalizes if there was an error on not. The error msg is stored in data.string
+} retval_s;
+
+/*
+ * Returns: The null terminated list of decks in data.stringv
+ */
+retval_s ac_get_decks(void);
+
+/*
+ * Returns: A null terminated list of notetypes in data.stringv
+ */
+retval_s ac_get_notetypes(void);
+
+/* 
+ * Search the Anki database for the entry @entry in the field @field and deck @deck.
+ * @deck can be null.
+ * The search result will be stored in data.boolen if there was no error.
+ */
+retval_s ac_search(_Bool include_suspended, char* deck, char* field, char* entry);
+
+/* 
+ * Performs a search as in ac_search, but in the Anki gui.
+ */
+retval_s ac_gui_search(const char* deck, const char* field, const char* entry);
+
+/*
+ * Stores the file at @path in the Anki media collection under the name @filename.
+ * Doesn't overwrite existing files.
+ */
+retval_s ac_store_file(char const* filename, char const* path);
+
+/* Add ankicard to anki  */
+retval_s ac_addNote(ankicard ac);
+
+#endif
--- include/database.h
+++ include/database.h
@@ -1,1 +1,22 @@
-../../jppron/include/database.h
+#include <stdbool.h>
+#include <lmdb.h>
+#include "util.h"
+
+typedef struct {
+  MDB_env *env;
+  MDB_dbi dbi1;
+  MDB_dbi dbi2;
+  MDB_txn *txn;
+  bool readonly;
+} database;
+
+database opendb(const char* path, bool readonly);
+void closedb(database);
+/*
+ * Add to database, allowing duplicates if they are added directly after another
+ */
+void addtodb1(database db, s8 key, s8 val);
+void addtodb2(database db, s8 key, s8 val);
+
+s8* getfiles(database db, s8 key);
+s8 getfromdb2(database db, s8 key);
