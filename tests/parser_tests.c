67b9edc31318da5746bb31ed5dbb13ee1924cad0
butterkeks <butterkeks@fedora.email>
2024-05-20 12:49:56 +0200

    Add more tests

--- tests/parser_tests.c
+++ tests/parser_tests.c
@@ -20,65 +20,50 @@ static bool dict_entries_equal(dictentry a, dictentry b) {
            a.frequency == b.frequency;
 }
 
-Ensure(Parser, skips_furigana) {
-    json_stream s[1];
-
-    const char *str_to_parse =
-        "[\"々\", \"のま\", \"\", \"\", 0, [{\"type\": \"structured-content\", \"content\": [{\"tag\": \"div\", \"content\": [{\"tag\": \"span\", \"style\": {\"verticalAlign\": \"text-bottom\", \"marginRight\": 0.25}, \"data\": {\"code\": \"unc\"}, \"content\": {\"tag\": \"img\", \"height\": 1.2, \"width\": 3.89, \"sizeUnits\": \"em\", \"appearance\": \"auto\", \"title\": \"unclassified\", \"collapsible\": false, \"collapsed\": false, \"background\": false, \"path\": \"jitendex/unclass.svg\"}}, {\"tag\": \"div\", \"content\": {\"tag\": \"ul\", \"style\": {\"marginBottom\": 0.5}, \"content\": [{\"tag\": \"li\", \"style\": {\"listStyleType\": \"square\", \"marginTop\": 0.5}, \"data\": {\"content\": \"info-gloss\"}, \"content\": \"kanji repetition mark\"}, {\"tag\": \"li\", \"style\": {\"fontSize\": \"120%\", \"listStyleType\": \"\\\"➡ \\\"\"}, \"data\": {\"content\": \"xref\"}, \"content\": [{\"tag\": \"span\", \"style\": {\"marginRight\": 0.25}, \"content\": \"see:\"}, {\"tag\": \"a\", \"lang\": \"ja\", \"href\": \"?query=同の字点&wildcards=off\", \"content\": [{\"tag\": \"ruby\", \"content\": [\"同\", {\"tag\": \"rt\", \"content\": \"どう\"}]}, \"の\", {\"tag\": \"ruby\", \"content\": [\"字\", {\"tag\": \"rt\", \"content\": \"じ\"}]}, {\"tag\": \"ruby\", \"content\": [\"点\", {\"tag\": \"rt\", \"content\": \"てん\"}]}]}]}, {\"tag\": \"li\", \"style\": {\"fontSize\": \"70%\", \"listStyleType\": \"none\"}, \"data\": {\"content\": \"xref-glossary\"}, \"content\": \"kanji iteration mark\"}]}}]}, {\"tag\": \"div\", \"data\": {\"content\": \"forms\"}, \"content\": [{\"tag\": \"span\", \"style\": {\"verticalAlign\": \"text-bottom\", \"marginRight\": 0.25}, \"content\": {\"tag\": \"img\", \"height\": 1.2, \"width\": 2.97, \"sizeUnits\": \"em\", \"appearance\": \"auto\", \"title\": \"spelling and reading variants\", \"collapsible\": false, \"collapsed\": false, \"background\": false, \"path\": \"jitendex/forms.svg\"}}, {\"tag\": \"ul\", \"content\": [{\"tag\": \"li\", \"content\": \"々\"}, {\"tag\": \"li\", \"content\": \"ノマ\"}]}]}]}], 1000060, \"\"]";
-    json_open_string(s, str_to_parse);
-    json_next(s);
-
-    dictentry parsed = parse_dictionary_entry(s);
-
-    const char *expected =
-        "▪ kanji repetition mark\n➡  see:同の字点\n  kanji iteration mark\n• 々\n• ノマ";
-    assert_that(parsed.definition.s, is_equal_to_string(expected));
-
-    dictentry_free(&parsed);
-    json_close(s);
-}
+#define CHECK_NR(n)                                                                                \
+    do {                                                                                           \
+        json_stream s[1];                                                                          \
+                                                                                                   \
+        char *str_to_parse = NULL;                                                                 \
+        g_file_get_contents("../tests/files/testentries/" #n "_entry", &str_to_parse, NULL, NULL); \
+        json_open_string(s, str_to_parse);                                                         \
+        json_next(s);                                                                              \
+        json_next(s);                                                                              \
+                                                                                                   \
+        dictentry parsed = parse_dictionary_entry(s);                                              \
+                                                                                                   \
+        char *expected = NULL;                                                                     \
+        g_file_get_contents("../tests/files/testentries/" #n "_expected", &expected, NULL, NULL);  \
+        g_strchomp(expected);                                                                      \
+        assert_that(parsed.definition.s, is_equal_to_string(expected));                            \
+                                                                                                   \
+        g_free(str_to_parse);                                                                      \
+        g_free(expected);                                                                          \
+        dictentry_free(&parsed);                                                                   \
+        json_close(s);                                                                             \
+    } while (0)
 
 Ensure(Parser, correctly_parses_structured_content1) {
-    json_stream s[1];
-
-    char *str_to_parse = NULL;
-    g_file_get_contents("../tests/files/testentries/1_entry", &str_to_parse, NULL, NULL);
-    json_open_string(s, str_to_parse);
-    json_next(s);
-
-    dictentry parsed = parse_dictionary_entry(s);
-
-    char *expected = NULL;
-    g_file_get_contents("../tests/files/testentries/1_expected", &expected, NULL, NULL);
-    assert_that(parsed.definition.s, is_equal_to_string(expected));
-
-    g_free(str_to_parse);
-    g_free(expected);
-    dictentry_free(&parsed);
-    json_close(s);
+    CHECK_NR(1);
 }
 
 Ensure(Parser, correctly_parses_structured_content2) {
-    json_stream s[1];
-
-    char *str_to_parse = NULL;
-    g_file_get_contents("../tests/files/testentries/2_entry", &str_to_parse, NULL, NULL);
-    json_open_string(s, str_to_parse);
-    json_next(s);
-
-    dictentry parsed = parse_dictionary_entry(s);
-
-    char *expected = NULL;
-    g_file_get_contents("../tests/files/testentries/2_expected", &expected, NULL, NULL);
-    assert_that(parsed.definition.s, is_equal_to_string(expected));
-
-    g_free(str_to_parse);
-    g_free(expected);
-    dictentry_free(&parsed);
-    json_close(s);
+    CHECK_NR(2);
 }
 
-Ensure(Parser, correctly_parses_frequency_entry) {
+Ensure(Parser, skips_furigana) {
+    CHECK_NR(3);
+}
+
+Ensure(Parser, includes_newlines_between_definitions1) {
+    CHECK_NR(4);
+}
+
+Ensure(Parser, includes_newlines_between_definitions2) {
+    CHECK_NR(5);
+}
+
+Ensure(Parser, correctly_parses_frequency_entry_with_reading) {
     json_stream s[1];
 
     const char *str_to_parse = "[\"糞\", \"freq\", {\"reading\": \"くそ\", \"frequency\": 9788}]";
@@ -95,11 +80,36 @@ Ensure(Parser, correctly_parses_frequency_entry) {
     json_close(s);
 }
 
+Ensure(Parser, correctly_parses_frequency_entry_without_reading) {
+    json_stream s[1];
+
+    const char *str_to_parse = "[\"私たち\", \"freq\", 581]";
+    json_open_string(s, str_to_parse);
+    json_next(s);
+
+    frequency_entry parsed = parse_frequency_entry(s);
+
+    frequency_entry expected = {.word = S("私たち"), .reading = (s8){0}, .frequency = 581};
+    bool equals = frequency_entries_equal(parsed, expected);
+    assert_that(equals, is_true);
+
+    frequency_entry_free(&parsed);
+    json_close(s);
+}
+
+Ensure(Parser, handles_tag_after_content) {
+    CHECK_NR(6);
+}
+
 TestSuite *parser_tests(void) {
     TestSuite *suite = create_test_suite();
     add_test_with_context(suite, Parser, skips_furigana);
+    add_test_with_context(suite, Parser, includes_newlines_between_definitions1);
+    add_test_with_context(suite, Parser, includes_newlines_between_definitions2);
     add_test_with_context(suite, Parser, correctly_parses_structured_content1);
     add_test_with_context(suite, Parser, correctly_parses_structured_content2);
-    add_test_with_context(suite, Parser, correctly_parses_frequency_entry);
+    add_test_with_context(suite, Parser, correctly_parses_frequency_entry_with_reading);
+    add_test_with_context(suite, Parser, correctly_parses_frequency_entry_without_reading);
+    // add_test_with_context(suite, Parser, handles_tag_after_content);
     return suite;
 }
