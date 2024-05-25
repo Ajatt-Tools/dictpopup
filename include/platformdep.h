e0b5d090b24ba608001f4a731131a23963916266
butterkeks <butterkeks@fedora.email>
2024-05-25 12:37:37 +0200

    Add include guard for platformdep

--- include/platformdep.h
+++ include/platformdep.h
@@ -1,3 +1,6 @@
+#ifndef PLATFORMDEP_H
+#define PLATFORMDEP_H
+
 #include "util.h"
 
 void _nonnull_n_(1) notify(const char *title, _Bool urgent, char const *fmt, ...);
@@ -8,3 +11,5 @@ void free_windowname(s8 windowname);
 void play_audio(s8 filepath);
 void _nonnull_ createdir(char *dirpath);
 const char *get_user_data_dir(void);
+
+#endif // PLATFORMDEP_H
