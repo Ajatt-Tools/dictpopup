4858b8768a7ae0885f5174a34aeda1903209dc88
butterkeks <butterkeks@fedora.email>
2024-08-25 15:24:52 +0200

    Add some ankiconnect tests

--- tests/ankiconnect_tests.c
+++ tests/ankiconnect_tests.c
@@ -10,7 +10,7 @@ TestSuite *ankiconnect_tests(void);
 
 retval_s sendRequest(s8 request, ResponseFunc response_checker) {
     mock(request.s);
-    return (retval_s){.ok=true};
+    return (retval_s){.ok = true};
 }
 
 Describe(AnkiConnectC);
@@ -30,22 +30,23 @@ Ensure(AnkiConnectC, sends_correct_guiBrowse_request) {
 
 Ensure(AnkiConnectC, sends_correct_search_request_without_suspended) {
     _drop_(frees8) s8 received = form_search_req(false, true, "Japanese", "VocabKanji", "敷衍");
-    const char* expected = "{ \"action\": \"findCards\", \"version\": 6, \"params\": { \"query\" : "
-                    "\"\\\"deck:Japanese\\\" \\\"VocabKanji:敷衍\\\" -is:suspended\" } }";
+    const char *expected = "{ \"action\": \"findCards\", \"version\": 6, \"params\": { \"query\" : "
+                           "\"\\\"deck:Japanese\\\" \\\"VocabKanji:敷衍\\\" -is:suspended\" } }";
     assert_that(received.s, is_equal_to_string(expected));
 }
 
 Ensure(AnkiConnectC, sends_correct_search_request_without_new) {
     _drop_(frees8) s8 received = form_search_req(true, false, "Japanese", "VocabKanji", "怒涛");
     const char *expected = "{ \"action\": \"findCards\", \"version\": 6, \"params\": { \"query\" : "
-                     "\"\\\"deck:Japanese\\\" \\\"VocabKanji:怒涛\\\" -is:new\" } }";
+                           "\"\\\"deck:Japanese\\\" \\\"VocabKanji:怒涛\\\" -is:new\" } }";
     assert_that(received.s, is_equal_to_string(expected));
 }
 
 Ensure(AnkiConnectC, sends_correct_search_request_without_new_and_suspended) {
     _drop_(frees8) s8 received = form_search_req(false, false, "Japanese", "VocabKanji", "寿命");
-    const char *expected = "{ \"action\": \"findCards\", \"version\": 6, \"params\": { \"query\" : "
-                     "\"\\\"deck:Japanese\\\" \\\"VocabKanji:寿命\\\" -is:suspended -is:new\" } }";
+    const char *expected =
+        "{ \"action\": \"findCards\", \"version\": 6, \"params\": { \"query\" : "
+        "\"\\\"deck:Japanese\\\" \\\"VocabKanji:寿命\\\" -is:suspended -is:new\" } }";
     assert_that(received.s, is_equal_to_string(expected));
 }
 
@@ -103,11 +104,16 @@ Ensure(AnkiConnectC, json_escapes_ankicard) {
     // TODO: Finish check
 }
 
+static retval_s call_callback_with(s8 response, ResponseFunc callback) {
+    retval_s ret = {0};
+    callback((char *)response.s, 1, response.len, &ret);
+    return ret;
+}
+
 Ensure(AnkiConnectC, properly_parses_string_array_response) {
     s8 response = S("{\"result\": [\"Basic\", \"Basic (and reversed card)\"], \"error\": null}");
 
-    retval_s ret = {0};
-    parse_result_array((char*)response.s, 0, response.len, &ret);
+    retval_s ret = call_callback_with(response, parse_result_array);
 
     char **parsed = ret.data.strv;
     assert_that(ret.ok, is_true);
@@ -117,14 +123,73 @@ Ensure(AnkiConnectC, properly_parses_string_array_response) {
     g_strfreev(ret.data.strv);
 }
 
-Ensure(AnkiConnectC, properly_handles_error_in_response) {
+Ensure(AnkiConnectC, properly_parses_error_1) {
     s8 response = S("{\"result\": null, \"error\": \"unsupported action\"}");
 
-    retval_s ret = {0};
-    parse_result_array((char*)response.s, 0, response.len, &ret);
+    retval_s ret = call_callback_with(response, check_error_callback);
 
     assert_that(ret.ok, is_false);
     assert_that(ret.data.string, is_equal_to_string("unsupported action"));
+
+    ac_retval_free(ret);
+}
+
+Ensure(AnkiConnectC, properly_parses_error_2) {
+    s8 response = S("{\"result\": null, \"error\": \"guiBrowse() got an unexpected keyword "
+                    "argument 'foobar'\"}");
+
+    retval_s ret = call_callback_with(response, check_error_callback);
+
+    assert_that(ret.ok, is_false);
+    assert_that(ret.data.string,
+                is_equal_to_string("guiBrowse() got an unexpected keyword argument 'foobar'"));
+
+    ac_retval_free(ret);
+}
+
+Ensure(AnkiConnectC, properly_parses_error_3) {
+    s8 response =
+        S("{ \"canAdd\": false, \"error\": \"cannot create note because it is a duplicate\" }");
+
+    retval_s ret = call_callback_with(response, check_error_callback);
+
+    assert_that(ret.ok, is_false);
+    assert_that(ret.data.string,
+                is_equal_to_string("cannot create note because it is a duplicate"));
+
+    ac_retval_free(ret);
+}
+
+Ensure(AnkiConnectC, properly_recognizes_existance_of_results_1) {
+    s8 response =
+        S("{ \"result\": [1494723142483, 1494703460437, 1494703479525], \"error\": null }");
+
+    retval_s ret = call_callback_with(response, has_results_callback);
+
+    assert_that(ret.ok, is_true);
+    assert_that(ret.data.boolean, is_true);
+
+    ac_retval_free(ret);
+}
+
+Ensure(AnkiConnectC, properly_recognizes_nonexistance_of_results) {
+    s8 response = S("{ \"result\": [], \"error\": null }");
+
+    retval_s ret = call_callback_with(response, has_results_callback);
+
+    assert_that(ret.ok, is_true);
+    assert_that(ret.data.boolean, is_false);
+
+    ac_retval_free(ret);
+}
+
+Ensure(AnkiConnectC, properly_parses_) {
+    s8 response = S("{ \"result\": [], \"error\": null }");
+
+    retval_s ret = call_callback_with(response, has_results_callback);
+
+    assert_that(ret.ok, is_true);
+    assert_that(ret.data.boolean, is_false);
 
     ac_retval_free(ret);
 }
@@ -134,10 +199,15 @@ TestSuite *ankiconnect_tests(void) {
     add_test_with_context(suite, AnkiConnectC, sends_correct_guiBrowse_request);
     add_test_with_context(suite, AnkiConnectC, sends_correct_search_request_without_suspended);
     add_test_with_context(suite, AnkiConnectC, sends_correct_search_request_without_new);
-    add_test_with_context(suite, AnkiConnectC, sends_correct_search_request_without_new_and_suspended);
+    add_test_with_context(suite, AnkiConnectC,
+                          sends_correct_search_request_without_new_and_suspended);
     add_test_with_context(suite, AnkiConnectC, sends_correct_addNote_request);
     add_test_with_context(suite, AnkiConnectC, json_escapes_ankicard);
     add_test_with_context(suite, AnkiConnectC, properly_parses_string_array_response);
-    add_test_with_context(suite, AnkiConnectC, properly_handles_error_in_response);
+    add_test_with_context(suite, AnkiConnectC, properly_parses_error_1);
+    add_test_with_context(suite, AnkiConnectC, properly_parses_error_2);
+    add_test_with_context(suite, AnkiConnectC, properly_parses_error_3);
+    add_test_with_context(suite, AnkiConnectC, properly_recognizes_existance_of_results_1);
+    add_test_with_context(suite, AnkiConnectC, properly_recognizes_nonexistance_of_results);
     return suite;
 }
